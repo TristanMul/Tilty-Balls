833c96f5dcf3f4db655a23268cd87c3b98aefe3e
lennartvandorp <57751002+lennartvandorp@users.noreply.github.com>
2021-05-20 15:59:00 +0200

    Begin of gameeventmanager

---
+++ Tilty Balls/Assets/Scripts/Managers/GameEventManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameEventManager : MonoBehaviour
+{
+    public static GameEventManager instance;
+
+    public event Action reachFinish;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReachFinish()
+    {
+        reachFinish();
+    }
+}
