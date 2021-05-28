9bcacc0eea7c6da2042ea6db81d346ca4aa94d5c
Fatih-Bagcaci <34387176+Fatih-Bagcaci@users.noreply.github.com>
2021-05-28 15:04:31 +0200

    Made a TimeStep controller
    Set Timestep at awake and onReachFinish.

---
+++ Tilty Balls/Assets/Scripts/Managers/TimeStepController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeStepController : MonoBehaviour
+{
+    private void Awake() {
+        RaiseTimeStep();
+    }
+
+    public void RaiseTimeStep(){
+        Time.fixedDeltaTime = 0.02f;
+    }
+
+    public void LowerTimeStep(){
+        Time.fixedDeltaTime = 0.002f;
+    }
+}
